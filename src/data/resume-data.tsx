c50be9e1a082731af7ec13f20f5fba46ef942949
Ammar <muhammad2000ammar@gmail.com>
2023-12-31 17:13:12 +0500

    first commit with personal data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -17,262 +17,289 @@ import {
   TastyCloudLogo,
   YearProgressLogo,
 } from "@/images/logos";
-import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+import { GitHubIcon, LinkedInIcon} from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Muhammad Ammar",
+  initials: "MA",
+  location: "Islamabad, Pakistan, PKT",
+  // locationLink: "https://www.google.com/maps/place/Wrocław",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Solution Analyst focused on developing automated solutions to solve applied problems",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "As a Solution Analyst, I have successfully developed multiple solutions to automate repeated tasks. My recent work has been focused at developing applications using LLMs. I am a lifelong learners, always improving myself step by step. Currently, I am focusing on improving my programming skills and learning about application deployment. ",
+  avatarUrl: "https://avatars.githubusercontent.com/Ammar2k",
+  personalWebsiteUrl: "ammar2k.github.io",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "muhammad2000ammar@gmail.com",
+    tel: "+923325755532",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/Ammar2k",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/muhammad2000ammar/",
         icon: LinkedInIcon,
       },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
-      },
+      // {
+      //   name: "X",
+      //   url: "https://x.com/BartoszJarocki",
+      //   icon: XIcon,
+      // },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "National University of Sciences and Technology",
+      degree: "Bachelor's Degree in Mechanical Engineering",
+      start: "2018",
+      end: "2022",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "Learners.ai",
+      link: "https://www.learners.ai/",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
+      title: "Solution Analyst",
+      // logo: ParabolLogo,
+      start: "2023",
       end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+      description: (
+        <>
+        <li>
+          Utilized LangChain and Pinecone to build a GPT-powered chatbot for generating sales proposals from call transcripts, incorporating insights from past proposals.
+        </li>
+        <li>
+          Developed a copywriter assistant chatbot using LangChain Agents and custom functions, along with a tailored knowledge base.
+        </li>
+        <li>
+          Conducted Data Import Analysis for data transfer from one CRM platform to another, using excel, SQL and Python.
+        </li>
+        <li>
+          Implemented a lead scoring model to prioritize potential clients and designed Data Flow diagrams for current and future data flows, suggesting improvements.
+        </li>
+        </>
+      ),
     },
+    // {
+    //   company: "Clevertech",
+    //   link: "https://clevertech.biz",
+    //   badges: ["Remote"],
+    //   title: "Lead Android Developer → Full Stack Developer",
+    //   logo: ClevertechLogo,
+    //   start: "2015",
+    //   end: "2021",
+    //   description:
+    //     "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+    // },
+    // {
+    //   company: "Jojo Mobile",
+    //   link: "https://bsgroup.eu/",
+    //   badges: [],
+    //   title: "Android Developer → Lead Android Developer",
+    //   logo: JojoMobileLogo,
+    //   start: "2012",
+    //   end: "2015",
+    //   description:
+    //     "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+    // },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
+      company: "Robotics and Intelligent Systems Engineering (RISE) Laboratory",
       link: "https://www.nokia.com",
       badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      title: "Undergraduate Researcher",
+      // logo: NSNLogo,
+      start: "2021",
+      end: "2022",
+      description: (
+        <>
+        <li>
+          Engaged in a senior year research project, providing bi-weekly presentations to a supervisory panel.
+        </li>
+        <li>
+          Developed a control algorithm for a mobile robot using Arduino, ultrasonic sensors, IR sensors, and encoders.
+        </li>
+        <li>
+          Successfully secured funding through IGNITE's National Grassroots Research Initiative 2022 and obtained funding for AHFE Conference registration fees under the Pakistan and European Union's Horizon 2020 ENHANCE project.
+        </li>
+        <li>
+          First Authored a conference paper for the Applied Human Factors and Ergonomics Conference (AHFE), 2022.{" "}
+          <a href="http://doi.org/10.54941/ahfe1001600">[1]</a>
+        </li>
+        </>
+      ),
     },
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Python",
+    "Pytorch",
+    "LLMs",
+    "Prompt Engineering",
+    "Computer Vision",
+    "Machine Learning",
+    "Data Science",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Autonomous Mobile Robot",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Robotics",
+        "Arduino Programming",
+        "Controls",
+        "Design"
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "A Chain-Driven Live Roller Mechanism for Loading and Unloading Packages on Autonomous Mobile Robots in Warehouses",
+      // logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "research paper link",
+        href: "http://doi.org/10.54941/ahfe1001600",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+      title: "Image2Audio app",
+      techStack: ["OpenAI API", "HuggingFace", "LangChain", "Streamlit", "TTS"],
+      description: "Users upload an image, the app generates a caption for the image, creates a short story based on the caption, and then converts the story into audio format.",
+      // logo: MonitoLogo,
       link: {
         label: "github.com",
-        href: "https://jarocki.me/",
+        href: "https://github.com/Ammar2k/image_to_audio-story",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Q&A Chatbot on private data",
+      techStack: ["OpenAI API", "LangChain", "Vector Database"],
       description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
+        "Blogpost on Retrieval Augmented Generation (RAG) using ChromaDB, GPT wrapped around LangChain, and private data.",
+      // logo: JarockiMeLogo,
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        label: "medium.com",
+        href: "https://medium.com/@muhammad2000ammar/building-a-q-a-chatbot-on-private-data-ba1af4a9a4dc",
       },
     },
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Scene Classification",
+      techStack: ["PyTorch", "Computer Vision", "Fine-Tuning", "Gradio"],
       description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
+        "Finetuned ViT pre-trained on ImageNet for Kaggle Intel Classification Challenge.",
+      // logo: Minimal,
       link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
+        label: "huggingface.co",
+        href: "https://huggingface.co/spaces/Ammar2k/scene_recognition/",
       },
     },
     {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
+      title: "Podcast Summarizer",
+      techStack: ["OpenAI API", "Whisper", "Audio2Text", "Streamlit"],
       description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+        "Converts podcast audio to text using whisper, uses GPT-3.5 to generate a summary, and uses wikipedia API to display additional information.",
+      // logo: BarepapersLogo,
       link: {
         label: "github.com",
-        href: "https://parabol.co/",
+        href: "https://github.com/Ammar2k/podcast_chatbot",
       },
     },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
+  //   {
+  //     title: "Year progress",
+  //     techStack: ["Side Project", "TypeScript", "Next.js"],
+  //     description: "Tracks current year progress and displays a countdown",
+  //     logo: YearProgressLogo,
+  //     link: {
+  //       label: "getyearprogress.com",
+  //       href: "https://getyearprogress.com/",
+  //     },
+  //   },
+  //   {
+  //     title: "Parabol",
+  //     techStack: [
+  //       "Full Stack Developer",
+  //       "TypeScript",
+  //       "React",
+  //       "Node.js",
+  //       "GraphQL",
+  //     ],
+  //     description:
+  //       "The Agile meeting co-pilot that delivers better meetings with less effort",
+  //     logo: ParabolLogo,
+  //     link: {
+  //       label: "github.com",
+  //       href: "https://parabol.co/",
+  //     },
+  //   },
+  //   {
+  //     title: "Evercast",
+  //     techStack: [
+  //       "Lead Frontend Developer",
 
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
+  //       "TypeScript",
+  //       "React",
+  //       "Node.js",
+  //       "GraphQL",
+  //     ],
+  //     description:
+  //       "Creative collaboration platform that combines video conferencing and HD media streaming",
+  //     logo: EvercastLogo,
+  //     link: {
+  //       label: "evercast.us",
+  //       href: "https://www.evercast.us/",
+  //     },
+  //   },
+  //   {
+  //     title: "Mobile Vikings",
+  //     techStack: ["Lead Android Developer", "Android", "Kotlin"],
+  //     description:
+  //       "Android application for leading virtual mobile operator in Poland",
+  //     logo: MobileVikingsLogo,
+  //     link: {
+  //       label: "mobilevikings.pl",
+  //       href: "https://mobilevikings.pl/",
+  //     },
+  //   },
+  //   {
+  //     title: "Howdy",
+  //     techStack: ["Lead Android Developer", "Android", "Kotlin"],
+  //     description:
+  //       "Howdy is a place for you to join communities you care about",
+  //     logo: Howdy,
+  //     link: {
+  //       label: "play.google.com",
+  //       href: "https://howdy.co/",
+  //     },
+  //   },
+  //   {
+  //     title: "Tastycloud",
+  //     techStack: ["Lead Android Developer", "Android", "Kotlin"],
+  //     description:
+  //       "Android application for managing and displaying restaurant menus in kiosk mode",
+  //     // logo: TastyCloudLogo,
+  //     link: {
+  //       label: "tastycloud.fr",
+  //       href: "https://www.tastycloud.fr/",
+  //     },
+  //   },
+  //   {
+  //     title: "Ambit",
+  //     techStack: ["Lead Android Developer", "Android", "Kotlin"],
+  //     description:
+  //       "Android application that helps with sharing your contact details",
+  //     // logo: AmbitLogo,
+  //   },
+  //   {
+  //     title: "Bim",
+  //     techStack: ["Lead Android Developer", "Android", "Kotlin"],
+  //     description:
+  //       "Android application that helps with booking a table in a restaurants",
+  //     logo: BimLogo,
+  //   },
+  //   {
+  //     title: "Canal Digital GO",
+  //     techStack: ["Lead Android Developer", "Android", "Kotlin"],
+  //     description:
+  //       "Video streaming mobile application for Canal Digital subscribers",
+  //     logo: CDGOLogo,
+  //   },
   ],
 } as const;
