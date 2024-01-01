0bf9703d1d65d9e43867dbe201f8a68e01f52c0b
Ammar <muhammad2000ammar@gmail.com>
2024-01-01 21:49:30 +0500

    fixing issues

--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+
+// const repo = 'https://github.com/Ammar2k/ammar2k.github.io'
+// const assetPrefix = `/${repo}/`
+// const basePath = `/${repo}`
+
+// module.exports = {
+//   assetPrefix: assetPrefix,
+//   basePath: basePath,
+// }
