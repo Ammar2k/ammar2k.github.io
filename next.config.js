325b758f30902e41069814bf2269c27864351445
Ammar <muhammad2000ammar@gmail.com>
2024-01-01 21:59:44 +0500

    updating next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,30 @@
-module.exports = {
-    // Other configurations...
+// module.exports = {
+//     // Other configurations...
 
-    // Add this line to enable the static HTML export
-    target: 'experimental-serverless-trace',
+//     // Add this line to enable the static HTML export
+//     target: 'experimental-serverless-trace',
 
-    // This is optional, but you can customize the exportPathMap if needed
-    exportPathMap: function () {
-    return {
-        '/': { page: '/' },
-        // Add other pages if needed
-        }
-    },
-}
+//     // This is optional, but you can customize the exportPathMap if needed
+//     exportPathMap: function () {
+//     return {
+//         '/': { page: '/' },
+//         // Add other pages if needed
+//         }
+//     },
+// };
 
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {}
 
 // module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+}
+
+module.exports = nextConfig
 
 
 // const repo = 'https://github.com/Ammar2k/ammar2k.github.io'
