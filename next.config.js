de0c19437493c9cfc62f971068d0198ad219e96f
Ammar <muhammad2000ammar@gmail.com>
2024-01-01 23:41:03 +0500

    updating config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    reactStrictMode: true,
 
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
+   // Add basePath
+    basePath: '/Ammar2k.github.io',
 
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
 }
 
 module.exports = nextConfig
